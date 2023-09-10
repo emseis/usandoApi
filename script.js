244c7103408e49bb8db24a0ec7d40caaf24f692f
emseis <emi.rodriguez9@outlook.com>
2023-09-10 19:46:00 -0300

    Pokemon

---
+++ script.js
@@ -0,0 +1,76 @@
+/*document.addEventListener("DOMContentLoaded", () => {
+    const pokemonInfo = document.getElementById("pokemon-info");
+    const pokemonInput = document.getElementById("pokemon-input");
+    const buscarButton = document.getElementById("buscar-button");
+
+    function obtenerInformacionPokemon(nombrePokemon) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon.toLowerCase()}`;
+
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("No se pudo obtener la información del Pokémon.");
+                }
+                return response.json();
+            })
+            .then(data => {
+                pokemonInfo.innerHTML = `
+                    <p>Nombre del Pokémon: ${data.name}</p>
+                    <p>ID del Pokémon: ${data.id}</p>
+                    <p>Tipos: ${data.types.map(type => type.type.name).join(", ")}</p>
+                `;
+            })
+            .catch(error => {
+                console.error(error);
+                pokemonInfo.textContent = "Error al obtener la información del Pokémon.";
+            });
+    }
+
+    buscarButton.addEventListener("click", () => {
+        const nombrePokemon = pokemonInput.value;
+        obtenerInformacionPokemon(nombrePokemon);
+    });
+});*/
+document.addEventListener("DOMContentLoaded", () => {
+    const pokemonInfo = document.getElementById("pokemon-info");
+    const pokemonInput = document.getElementById("pokemon-input");
+    const buscarButton = document.getElementById("buscar-button");
+    const nombrePokemonSpan = document.getElementById("nombre-pokemon");
+    const idPokemonSpan = document.getElementById("id-pokemon");
+    const tiposPokemonSpan = document.getElementById("tipos-pokemon");
+    const imagenPokemon = document.getElementById("imagen-pokemon");
+
+    function obtenerInformacionPokemon(nombrePokemon) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon.toLowerCase()}`;
+
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("No se pudo obtener la información del Pokémon.");
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Mostrar información del Pokémon
+                nombrePokemonSpan.textContent = data.name;
+                idPokemonSpan.textContent = data.id;
+                tiposPokemonSpan.textContent = data.types.map(type => type.type.name).join(", ");
+
+                // Obtener la URL de la imagen del Pokémon
+                const imagenUrl = data.sprites.front_default;
+
+                // Mostrar la imagen del Pokémon
+                imagenPokemon.src = imagenUrl;
+                imagenPokemon.alt = `Imagen de ${data.name}`;
+            })
+            .catch(error => {
+                console.error(error);
+                pokemonInfo.textContent = "Error al obtener la información del Pokémon.";
+            });
+    }
+
+    buscarButton.addEventListener("click", () => {
+        const nombrePokemon = pokemonInput.value;
+        obtenerInformacionPokemon(nombrePokemon);
+    });
+});
